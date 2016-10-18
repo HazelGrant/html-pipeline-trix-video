ee278cdd81ebd5bac0d3547f14408f47915f576f
WendyBeth <wendybeth010@gmail.com>
2016-10-18 16:47:45 -0400

    Adds a file that follows convention for file loading
    Removes development dependencies as *actual* dependencies.

---
+++ lib/html/pipeline/trix/video.rb
@@ -0,0 +1,1 @@
+require "html/pipeline/trix_video"
--- lib/html/pipeline/trix_video.rb
+++ lib/html/pipeline/trix_video.rb
@@ -1,5 +1,2 @@
-require "pry"
-require "awesome_print"
-
 require "html/pipeline/trix_video/version"
 require "html/pipeline/trix_video/trix_video_filter"
